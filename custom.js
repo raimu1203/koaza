35bf4bc6307ff2ccfd372d136d4338cceb202272
raimu1203 <raimu.1203.sr@icloud.com>
2026-07-06 00:36:41 +0900

    Add style function for lyr_map_1

--- custom.js
+++ custom.js
@@ -1,1 +1,11 @@
-console.log("custom.js 読み込み成功");
+lyr_map_1.setStyle(function(feature, resolution) {
+    return new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: 'rgba(255,0,0,0.3)'
+        }),
+        stroke: new ol.style.Stroke({
+            color: 'rgba(255,0,0,1.0)',
+            width: 1
+        })
+    });
+});
