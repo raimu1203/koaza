ed8a33aad9c12c2714e4c6ad70e1d56704040a0f
raimu1203 <raimu.1203.sr@icloud.com>
2026-07-06 00:33:49 +0900

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,10 +1,1 @@
-var originalStyle = lyr_map_1.getStyle();
-
-lyr_map_1.setStyle(function(feature, resolution) {
-    var style = originalStyle(feature, resolution);
-
-    // 塗りだけ変更
-    style.getFill().setColor('rgba(255,0,0,0.3)');
-
-    return style;
-});
+console.log("custom.js 読み込み成功");
