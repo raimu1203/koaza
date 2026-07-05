c0f3e269a73c9e452edc16cbe0a90507bd306c18
raimu1203 <raimu.1203.sr@icloud.com>
2026-07-06 00:45:24 +0900

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,7 +1,9 @@
 lyr_map_1.setStyle(function(feature, resolution) {
+    var color = feature.get('color'); // 属性に色がある場合
+
     return new ol.style.Style({
         fill: new ol.style.Fill({
-            color: 'rgba(255,0,0,0.3)'
+            color: 'rgba(255,0,0,0.3)'  // ←ここを動的にしてもOK
         }),
         stroke: new ol.style.Stroke({
             color: 'rgba(255,0,0,1.0)',
