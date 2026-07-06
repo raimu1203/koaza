ed6cf155aae81fe76cdef1a90d9be8a61c108993
raimu1203 <raimu.1203.sr@icloud.com>
2026-07-07 00:24:11 +0900

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -11,47 +11,47 @@ var wms_layers = [];
                 url: 'https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png'
             })
         });
-var format__1 = new ol.format.GeoJSON();
-var features__1 = format__1.readFeatures(json__1, 
+var format_Sendai_Koaza_Map_1 = new ol.format.GeoJSON();
+var features_Sendai_Koaza_Map_1 = format_Sendai_Koaza_Map_1.readFeatures(json_Sendai_Koaza_Map_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource__1 = new ol.source.Vector({
+var jsonSource_Sendai_Koaza_Map_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource__1.addFeatures(features__1);
-var lyr__1 = new ol.layer.Vector({
+jsonSource_Sendai_Koaza_Map_1.addFeatures(features_Sendai_Koaza_Map_1);
+var lyr_Sendai_Koaza_Map_1 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource__1, 
-                style: style__1,
-                popuplayertitle: '仙台市小字地図',
+                source:jsonSource_Sendai_Koaza_Map_1, 
+                style: style_Sendai_Koaza_Map_1,
+                popuplayertitle: 'Sendai_Koaza_Map',
                 interactive: true,
-    title: '仙台市小字地図<br />\
-    <img src="styles/legend/_1_0.png" /> 仙台<br />\
-    <img src="styles/legend/_1_1.png" /> 南小泉<br />\
-    <img src="styles/legend/_1_2.png" /> <br />' });
-var format__2 = new ol.format.GeoJSON();
-var features__2 = format__2.readFeatures(json__2, 
+    title: 'Sendai_Koaza_Map<br />\
+    <img src="styles/legend/Sendai_Koaza_Map_1_0.png" /> 仙台<br />\
+    <img src="styles/legend/Sendai_Koaza_Map_1_1.png" /> 南小泉<br />\
+    <img src="styles/legend/Sendai_Koaza_Map_1_2.png" /> <br />' });
+var format_Sendai_Oaza_Map_2 = new ol.format.GeoJSON();
+var features_Sendai_Oaza_Map_2 = format_Sendai_Oaza_Map_2.readFeatures(json_Sendai_Oaza_Map_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource__2 = new ol.source.Vector({
+var jsonSource_Sendai_Oaza_Map_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource__2.addFeatures(features__2);
-var lyr__2 = new ol.layer.Vector({
+jsonSource_Sendai_Oaza_Map_2.addFeatures(features_Sendai_Oaza_Map_2);
+var lyr_Sendai_Oaza_Map_2 = new ol.layer.Vector({
                 declutter: false,
-                source:jsonSource__2, 
-                style: style__2,
-                popuplayertitle: '仙台市大字地図',
+                source:jsonSource_Sendai_Oaza_Map_2, 
+                style: style_Sendai_Oaza_Map_2,
+                popuplayertitle: 'Sendai_Oaza_Map',
                 interactive: false,
-                title: '<img src="styles/legend/_2.png" /> 仙台市大字地図'
+                title: '<img src="styles/legend/Sendai_Oaza_Map_2.png" /> Sendai_Oaza_Map'
             });
 
-lyr__0.setVisible(true);lyr__1.setVisible(true);lyr__2.setVisible(true);
-var layersList = [lyr__0,lyr__1,lyr__2];
-lyr__1.set('fieldAliases', {'大字': '大字', '小字': '小字', });
-lyr__2.set('fieldAliases', {'大字': '大字', });
-lyr__1.set('fieldImages', {'大字': 'TextEdit', '小字': 'TextEdit', });
-lyr__2.set('fieldImages', {'大字': 'TextEdit', });
-lyr__1.set('fieldLabels', {'大字': 'inline label - visible with data', '小字': 'inline label - always visible', });
-lyr__2.set('fieldLabels', {'大字': 'no label', });
-lyr__2.on('precompose', function(evt) {
+lyr__0.setVisible(true);lyr_Sendai_Koaza_Map_1.setVisible(true);lyr_Sendai_Oaza_Map_2.setVisible(true);
+var layersList = [lyr__0,lyr_Sendai_Koaza_Map_1,lyr_Sendai_Oaza_Map_2];
+lyr_Sendai_Koaza_Map_1.set('fieldAliases', {'大字': '大字', '小字': '小字', });
+lyr_Sendai_Oaza_Map_2.set('fieldAliases', {'大字': '大字', });
+lyr_Sendai_Koaza_Map_1.set('fieldImages', {'大字': 'TextEdit', '小字': 'TextEdit', });
+lyr_Sendai_Oaza_Map_2.set('fieldImages', {'大字': 'TextEdit', });
+lyr_Sendai_Koaza_Map_1.set('fieldLabels', {'大字': 'inline label - visible with data', '小字': 'inline label - always visible', });
+lyr_Sendai_Oaza_Map_2.set('fieldLabels', {'大字': 'no label', });
+lyr_Sendai_Oaza_Map_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
