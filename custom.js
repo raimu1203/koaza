33b2cff93e8f34b5684ade49329e520239aaa0f4
raimu1203 <raimu.1203.sr@icloud.com>
2026-07-07 00:19:34 +0900

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 // ページが完全に読み込まれた後に実行する
 window.addEventListener('DOMContentLoaded', () => {
     // 💡 2行目の「lyr_1」の「1」の部分は、先ほどリネームしたファイル名（1.js）に対応しています
-    if (typeof lyr_1 !== 'undefined') {
+    if (typeof lyr__1 !== 'undefined') {
         // 透明度を 0.5 (50%) に設定する
         lyr_1.setOpacity(0.5);
         console.log("レイヤー1の透明度を0.5に変更しました。");
